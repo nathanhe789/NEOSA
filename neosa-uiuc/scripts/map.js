1940bddac23fdd11d7053c493d4682a084e9f7b9
Jonathan Reynolds <j14reynolds@gmail.com>
2015-10-31 15:04:45 -0500

    Readying XMPP services

--- neosa-uiuc/scripts/map.js
+++ neosa-uiuc/scripts/map.js
@@ -17,6 +17,10 @@ function initialize() {
   setInterval(getAllUsersLatLng, 30000);
 }
 
+$( window ).unload(function() {
+  jQuery.get("/logout");
+});
+
 function getUserLocation() {
   if (navigator.geolocation){
     /*
