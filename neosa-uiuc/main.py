b194948945ec8659c1a0a5eb9d14e33483f9697a
Nathan He <nathanhe@urhwirelessprvnat-172-18-3-70.near.illinois.edu>
2015-11-13 16:15:58 -0600

    fixed internal networking

--- neosa-uiuc/main.py
+++ neosa-uiuc/main.py
@@ -132,10 +132,18 @@ class SubjectHandler(webapp2.RequestHandler):
         user.put()
         self.redirect('/')
 
+class ProfilePageHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('templates/profilepage69.html')
+        self.response.out.write(template.render())
+    def post(self):
+        user = getCurrentUser.get()
+
 app = webapp2.WSGIApplication([
     ('/map', MapHandler),
     ('/users', UsersHandler),
     ('/profile', ProfileHandler),
+    ('/profilepage69', ProfilePageHandler),
     ('/login', LoginHandler),
     ('/schedule', ScheduleHandler),
     ('/test', Test),
